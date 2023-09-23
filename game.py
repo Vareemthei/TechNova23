8cb911e878877042d23ce052a338f0a360009236
Mary Grace <dmakmakmg@gmail.com>
2023-09-23 12:18:58 -0400

    fixed file directory restructure

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
-import pygame
 import time
 import random
-from settings import *
-from background import Background
-from hand import Hand
-from hand_tracking import HandTracking
-from mosquito import Mosquito
-from bee import Bee
+
+import pygame
 import cv2
-import ui
+
+import window.ui as ui
+from window.settings import *
+from window.background import Background
+from movement_tracking.hand import Hand
+from movement_tracking.hand_tracking import HandTracking
+
 
 class Game:
     def __init__(self, surface):
@@ -21,11 +22,11 @@ class Game:
         self.sounds = {}
         self.sounds["slap"] = pygame.mixer.Sound(f"Assets/Sounds/slap.wav")
         self.sounds["slap"].set_volume(SOUNDS_VOLUME)
-        self.sounds["screaming"] = pygame.mixer.Sound(f"Assets/Sounds/screaming.wav")
+        self.sounds["screaming"] = pygame.mixer.Sound(
+            f"Assets/Sounds/screaming.wav")
         self.sounds["screaming"].set_volume(SOUNDS_VOLUME)
 
-
-    def reset(self): # reset all the needed variables
+    def reset(self):  # reset all the needed variables
         self.hand_tracking = HandTracking()
         self.hand = Hand()
         self.insects = []
@@ -33,26 +34,8 @@ class Game:
         self.score = 0
         self.game_start_time = time.time()
 
-
-    def spawn_insects(self):
-        t = time.time()
-        if t > self.insects_spawn_timer:
-            self.insects_spawn_timer = t + MOSQUITOS_SPAWN_TIME
-
-            # increase the probability that the insect will be a bee over time
-            nb = (GAME_DURATION-self.time_left)/GAME_DURATION * 100  / 2  # increase from 0 to 50 during all  the game (linear)
-            if random.randint(0, 100) < nb:
-                self.insects.append(Bee())
-            else:
-                self.insects.append(Mosquito())
-
-            # spawn a other mosquito after the half of the game
-            if self.time_left < GAME_DURATION/2:
-                self.insects.append(Mosquito())
-
     def load_camera(self):
         _, self.frame = self.cap.read()
-
 
     def set_hand_position(self):
         self.frame = self.hand_tracking.scan_hands(self.frame)
@@ -69,17 +52,17 @@ class Game:
         self.hand.draw(self.surface)
         # draw the score
         ui.draw_text(self.surface, f"Score : {self.score}", (5, 5), COLORS["score"], font=FONTS["medium"],
-                    shadow=True, shadow_color=(255,255,255))
+                     shadow=True, shadow_color=(255, 255, 255))
         # draw the time left
-        timer_text_color = (160, 40, 0) if self.time_left < 5 else COLORS["timer"] # change the text color if less than 5 s left
+        # change the text color if less than 5 s left
+        timer_text_color = (
+            160, 40, 0) if self.time_left < 5 else COLORS["timer"]
         ui.draw_text(self.surface, f"Time left : {self.time_left}", (SCREEN_WIDTH//2, 5),  timer_text_color, font=FONTS["medium"],
-                    shadow=True, shadow_color=(255,255,255))
-
+                     shadow=True, shadow_color=(255, 255, 255))
 
     def game_time_update(self):
-        self.time_left = max(round(GAME_DURATION - (time.time() - self.game_start_time), 1), 0)
-
-
+        self.time_left = max(
+            round(GAME_DURATION - (time.time() - self.game_start_time), 1), 0)
 
     def update(self):
 
@@ -99,14 +82,14 @@ class Game:
                 self.hand.image = self.hand.image_smaller.copy()
             else:
                 self.hand.image = self.hand.orig_image.copy()
-            self.score = self.hand.kill_insects(self.insects, self.score, self.sounds)
+            self.score = self.hand.kill_insects(
+                self.insects, self.score, self.sounds)
             for insect in self.insects:
                 insect.move()
 
-        else: # when the game is over
+        else:  # when the game is over
             if ui.button(self.surface, 540, "Continue", click_sound=self.sounds["slap"]):
                 return "menu"
-
 
         cv2.imshow("Frame", self.frame)
         cv2.waitKey(1)
